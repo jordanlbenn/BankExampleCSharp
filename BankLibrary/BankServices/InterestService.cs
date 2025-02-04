f5f02d91fff21ff977db7d5fa18100ebecf57596
Jordan Bennett <156590653+jordanlbenn@users.noreply.github.com>
2025-02-04 02:31:02 -0500

    Left off here
    OOP Example Bank

---
+++ BankLibrary/BankServices/InterestService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankServices
+{
+    public class InterestService
+    {
+        public void ApplyInterestToAccounts(List<Account> accounts)
+        {
+            foreach (var account in accounts)
+            {
+                if (account is SavingsAccount savingsAccount)
+                {
+                    savingsAccount.ApplyInterest();
+                    Console.WriteLine($"Interest applied to SavingsAccount ({savingsAccount.AccountNumber}} New Balance {savingsAccount.Balance:C}");
+                }
+            }
+         }
+    }
+}
